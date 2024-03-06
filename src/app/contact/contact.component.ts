642b4c0dac123e913754e7f093e1240124a0bd0c
Nitin <nitinaswal68@gmail.com>
2024-03-06 18:00:52 +0530

    email body change

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,13 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  @ViewChild('numberOfStaysInput')
+  numberOfStaysInput!: ElementRef;
+  @ViewChild('name')
+  name!: ElementRef;
+  @ViewChild('email_Id')
+  email_Id!: ElementRef;
+  @ViewChild('phoneNumber')
+  phoneNumber!: ElementRef;
+  @ViewChild('arrivalDate')
+  arrivalDate!: ElementRef;
+  @ViewChild('message')
+  message!: ElementRef;
   emailId = "nitinaswal68@gmail.com";
   constructor() { }
   submitForm() {
     const email = 'nitinaswal68@gmail.com';
     const subject = 'Contact Form Submission';
-    const body = 'Please find ';
-
+    const numberOfStaysValue = this.numberOfStaysInput.nativeElement.value;
+    const nameValue = this.name.nativeElement.value;
+    const emailIdValue = this.email_Id.nativeElement.value;
+    const phoneNumberValue = this.phoneNumber.nativeElement.value;
+    const arrivalDateValue = this.arrivalDate.nativeElement.value;
+    const messageValue = this.message.nativeElement.value;
+    const body = `Hi Nitin ,\n Please find the details for the booking:\n\n
+    Name: ${nameValue}\n
+    Phone Number: ${phoneNumberValue}\n
+    Email ID: ${emailIdValue}\n
+    Arrival Date: ${arrivalDateValue}\n
+    Number of Night Stays: ${numberOfStaysValue}\n
+    Message: ${messageValue}\n
+    Have a nice day.`;
     // Generate mailto link
     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
